561f17a80845eccc4d2726904f89b0367dd0f8c2
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2024-10-09 22:59:56 +0900

    feat: interact raycast

--- Characters/CharacterPlayer.cs
+++ Characters/CharacterPlayer.cs
@@ -2,7 +2,9 @@ using Godot;
 
 public partial class CharacterPlayer : Character
 {
+    [Export] public new int Speed { get; set; } = 300;
     [Export] private NodePath inventoryUIPath;
+    [Export] private NodePath spritePath;
     private CanvasLayer inventoryUI;
 
     public override void _Ready()
@@ -18,6 +20,10 @@ public partial class CharacterPlayer : Character
         if (@event.IsActionPressed("ui_inventory"))
         {
             inventoryUI.Visible = !inventoryUI.Visible;
+        }
+        if (@event.IsActionPressed("interact"))
+        {
+            Interact();
         }
     }
 
@@ -62,6 +68,30 @@ public partial class CharacterPlayer : Character
                 Button button = new Button();
                 button.CustomMinimumSize = new Vector2(64, 64);
                 inventoryGrid.AddChild(button);
+            }
+        }
+    }
+
+    public void Interact()
+    {
+        // Create a ray to detect objects in front of the player (for example)
+        var spaceState = GetWorld2D().DirectSpaceState;
+        var rayOrigin = GlobalPosition;
+        var currentDirection = GetNode<Sprite2D>(spritePath).GlobalRotation;
+        var rayEnd = GlobalPosition + new Vector2(1, 0).Rotated(currentDirection) * 64;
+
+        // Perform the raycast
+        var result = spaceState.IntersectRay(PhysicsRayQueryParameters2D.Create(rayOrigin, rayEnd));
+
+        if (result != null && result.ContainsKey("collider"))
+        {
+            var collider = (Node2D)result["collider"];
+
+            // Check if the object is in the "interactable" group and call its interaction method
+            if (collider.IsInGroup("interactable"))
+            {
+                collider.Call("Interact");
+                GD.Print("Interacted with " + collider.Name);
             }
         }
     }
